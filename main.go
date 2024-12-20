142ca5b757203636f19f885f25e8101cd2a54ab5
alkakhushbu <alkakhushbu@gmail.com>
2024-12-20 15:06:38 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", ping)
+	router.Run(":80")
+}
+
+func ping(c *gin.Context) {
+	c.JSON(http.StatusOK, "Hello ping")
+}
